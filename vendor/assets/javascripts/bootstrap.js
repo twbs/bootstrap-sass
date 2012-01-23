972d254724ab82116b902ff33b5c8b3264f7d633
Mark Kremer <mark@without-brains.net>
2012-01-23 15:29:40 +0100

    Rails development mode is sensitive to the order in which dependencies are included

--- vendor/assets/javascripts/bootstrap.js
+++ vendor/assets/javascripts/bootstrap.js
@@ -1,9 +1,8 @@
 //= require bootstrap-modal
 //= require bootstrap-buttons
-//= require bootstrap-dropdown
 //= require bootstrap-scrollspy
 //= require bootstrap-tabs
+//= require bootstrap-dropdown
 //= require bootstrap-twipsy
 //= require bootstrap-popover
 //= require bootstrap-alerts
-
