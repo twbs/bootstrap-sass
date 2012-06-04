349cd8ed4e52011ecaca16ffece99eab7502e14c
Thomas McDonald <tom@conceptcoding.co.uk>
2012-06-04 11:45:25 +0100

    update tests to compile bootstrap-responsive

--- test/compilation_test.rb
+++ test/compilation_test.rb
@@ -3,9 +3,11 @@ require 'test_helper'
 class CompilationTest < Test::Unit::TestCase
   def test_compilation
     path = 'vendor/assets/stylesheets'
-    engine = Sass::Engine.for_file("#{path}/_bootstrap.scss", syntax: :scss, load_paths: [path])
-    assert_nothing_raised do
-      engine.render
+    %w(bootstrap bootstrap-responsive).each do |file|
+      engine = Sass::Engine.for_file("#{path}/_#{file}.scss", syntax: :scss, load_paths: [path])
+      assert_nothing_raised do
+        engine.render
+      end
     end
   end
 end
