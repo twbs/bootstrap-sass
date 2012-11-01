6dea3d599fce2d4afa05ebdf222cd5a5be87474f
Tristan Harward <trisweb@gmail.com>
2012-11-01 11:04:42 -0400

    Fix module to correctly include compass extenstion.
    Sorry about all the commits - still getting my bearings with compass.

--- lib/bootstrap-sass/compass_functions.rb
+++ lib/bootstrap-sass/compass_functions.rb
@@ -1,6 +1,6 @@
 # This contains functions for use with a project *only* using Compass.
 
-module Compass::SassExtensions::Functions::Urls
+module Sass::Script::Functions
   module ImageUrl
     # Define image_path for Compass to allow use of sprites without url() wrapper.
     def image_path(asset)
