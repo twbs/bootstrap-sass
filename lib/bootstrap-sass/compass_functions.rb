2cca8870c2ce84c4af283b99f21bb4b68c6954ce
Tristan Harward <trisweb@gmail.com>
2012-11-01 11:06:04 -0400

    Bah, don't need the submodule anymore.

--- lib/bootstrap-sass/compass_functions.rb
+++ lib/bootstrap-sass/compass_functions.rb
@@ -1,10 +1,8 @@
 # This contains functions for use with a project *only* using Compass.
 
 module Sass::Script::Functions
-  module ImageUrl
-    # Define image_path for Compass to allow use of sprites without url() wrapper.
-    def image_path(asset)
-      image_url(asset, Sass::Script::Bool.new(true))
-    end
+  # Define image_path for Compass to allow use of sprites without url() wrapper.
+  def image_path(asset)
+    image_url(asset, Sass::Script::Bool.new(true))
   end
 end
