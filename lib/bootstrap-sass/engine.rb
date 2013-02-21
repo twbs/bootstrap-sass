522cb3bd974b463a72529d03aa01747fc8ec3cd4
millisami <millisami@gmail.com>
2013-02-21 21:57:18 +0545

    Update lib/bootstrap-sass/engine.rb
    When trying to use with non-rails app like sinatra, 
    this blows out with undefined constant Rails

--- lib/bootstrap-sass/engine.rb
+++ lib/bootstrap-sass/engine.rb
@@ -1,7 +1,9 @@
-module Bootstrap
-  module Rails
-    class Engine < ::Rails::Engine
-      # Rails, will you please look in our vendor? kthx
+if defined?(Rails) #Conditional check if the gem is used without Rails
+  module Bootstrap
+    module Rails
+      class Engine < ::Rails::Engine
+        # Rails, will you please look in our vendor? kthx
+      end
     end
   end
 end
