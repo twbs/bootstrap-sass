1cd27194c984008e7ab70e4d3a07be041935843f
Teddy Caddy <info@teddycaddy.com>
2013-03-06 13:16:26 -0500

    modify update-bootstrap.sh to use non-standard repo/branch for sass-twitter-bootstrap

--- update-bootstrap.sh
+++ update-bootstrap.sh
@@ -2,8 +2,17 @@
 
 ROOT=`pwd`"/vendor/assets"
 TMP='tmp/sass-twitter-bootstrap'
+
+# the normal sass-twitter-bootstrap repo hasn't been updated to 3.2.1 yet
+# REPO='https://github.com/jlong/sass-twitter-bootstrap.git'
+# BRANCH='master'
+
+# here is a forked repo that has been updated to 3.2.1
+REPO='https://github.com/roverwolf/sass-twitter-bootstrap.git'
+BRANCH='2.3.1-updates'
+
 # Pull down sass-twitter-bootstrap sources
-git clone https://github.com/jlong/sass-twitter-bootstrap.git tmp/sass-twitter-bootstrap
+git clone -b $BRANCH $REPO tmp/sass-twitter-bootstrap
 # Copy lib/ to stylesheets/
 mkdir -p $ROOT/stylesheets/bootstrap
 cp -r $TMP/lib/* $ROOT/stylesheets/bootstrap
