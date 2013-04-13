fdf5e92ac29fd90912f78ed8931f35311ddae4f4
Thomas McDonald <tom@conceptcoding.co.uk>
2013-04-13 16:00:54 +0100

    Fix tests lazily
    This wants taking out of the array, both here and in the Rakefile

--- test/compilation_test.rb
+++ test/compilation_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class CompilationTest < Test::Unit::TestCase
   def test_compilation
     path = 'vendor/assets/stylesheets'
-    %w(bootstrap bootstrap-responsive).each do |file|
+    %w(bootstrap).each do |file|
       engine = Sass::Engine.for_file("#{path}/#{file}.scss", syntax: :scss, load_paths: [path])
       assert_nothing_raised do
         engine.render
