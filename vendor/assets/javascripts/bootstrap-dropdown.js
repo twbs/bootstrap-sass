83c5f0abb06a8cf72de197227d2db705085daf82
haynescw <haynescw@uwec.edu>
2013-05-21 16:52:51 -0500

    Update bootstrap-dropdown.js

--- vendor/assets/javascripts/bootstrap-dropdown.js
+++ vendor/assets/javascripts/bootstrap-dropdown.js
@@ -156,7 +156,7 @@
    * =================================== */
 
 
-  $(document)
+   $(document)
 //*** This is the original line
 //    .on('click.dropdown.data-api touchstart.dropdown.data-api', clearMenus) 
 //*** This is the modified line to re-enable dropdowns on iPads and iPhones.
@@ -164,9 +164,6 @@
     .on('click.dropdown touchstart.dropdown.data-api', '.dropdown form', function (e) { e.stopPropagation() })
     .on('click.dropdown.data-api touchstart.dropdown.data-api'  , toggle, Dropdown.prototype.toggle)
     .on('keydown.dropdown.data-api touchstart.dropdown.data-api', toggle + ', [role=menu]' , Dropdown.prototype.keydown)
-    .on('click.dropdown.data-api', '.dropdown form', function (e) { e.stopPropagation() })
-    .on('click.dropdown-menu', function (e) { e.stopPropagation() })
-    .on('click.dropdown.data-api'  , toggle, Dropdown.prototype.toggle)
-    .on('keydown.dropdown.data-api', toggle + ', [role=menu]' , Dropdown.prototype.keydown)
+
 
 }(window.jQuery);
