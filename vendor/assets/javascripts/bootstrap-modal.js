da91bc1840d39c75e6612f2268685024a48a6f40
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2013-05-22 16:30:03 -0700

    Apply upstream commit to fix loading remote modals

--- vendor/assets/javascripts/bootstrap-modal.js
+++ vendor/assets/javascripts/bootstrap-modal.js
@@ -30,7 +30,11 @@
     this.options = options
     this.$element = $(element)
       .delegate('[data-dismiss="modal"]', 'click.dismiss.modal', $.proxy(this.hide, this))
-    this.options.remote && this.$element.find('.modal-body').load(this.options.remote)
+    if (this.$element.find('.modal-body').length == 0) {
+      this.options.remote && this.$element.load(this.options.remote)
+    } else {
+      this.options.remote && this.$element.find('.modal-body').load(this.options.remote)
+    }
   }
 
   Modal.prototype = {
