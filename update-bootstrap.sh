9ef003c3b579369782aee5cdc0162e9254f70ca0
gleb <glex.spb@gmail.com>
2013-05-28 18:37:56 +0200

    sort out javascripts

--- update-bootstrap.sh
+++ update-bootstrap.sh
@@ -7,19 +7,21 @@ git clone https://github.com/jlong/sass-twitter-bootstrap.git tmp/sass-twitter-b
 # Copy lib/ to stylesheets/
 mkdir -p $ROOT/stylesheets/bootstrap
 cp -r $TMP/lib/* $ROOT/stylesheets/bootstrap
-# Copy js/ to javascripts/
-cp -r $TMP/js/* $ROOT/javascripts
+# Copy js/ to javascripts/bootstrap
+cp -r $TMP/js/* $ROOT/javascripts/bootstrap
 # Copy img/ to images/
 cp -r $TMP/img/* $ROOT/images
+# Copy fonts/ to fonts/
+cp -r $TMP/fonts/* $ROOT/fonts
 # Remove tests
-rm -r $ROOT/javascripts/tests
-rm -r $ROOT/stylesheets/bootstrap/tests
+rm -r $ROOT/javascripts/bootstrap/tests
 
 # Patch the asset-url in _glyphicons.scss
 patch -f vendor/assets/stylesheets/bootstrap/_glyphicons.scss < asseturl.patch
+rm $ROOT/stylesheets/bootstrap/_glyphicons.scss.orig
 
 # Patch paths in bootstrap.scss and responsive.scss
-sed -i .bak 's_@import \"_@import \"bootstrap/_g' $ROOT/stylesheets/bootstrap/{bootstrap,responsive}.scss
+sed -i .bak 's_@import \"_@import \"bootstrap/_g' $ROOT/stylesheets/bootstrap/bootstrap.scss
 rm $ROOT/stylesheets/bootstrap/*.bak
 
 rm -rf $TMP
