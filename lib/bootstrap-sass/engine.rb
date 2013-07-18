516da91eae1861ce708aa1ef5a44539d314d9f7f
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2013-07-18 11:49:29 -0300

    Plug on Rails to tell it to precompile our own images
    Rails changed the way we automatically precompile non-javascript/css files
    to make easier to third-party plugins to choose which files they want to
    precompile instead of precompiling everything under vendor or lib.

--- lib/bootstrap-sass/engine.rb
+++ lib/bootstrap-sass/engine.rb
@@ -1,7 +1,9 @@
 module Bootstrap
   module Rails
     class Engine < ::Rails::Engine
-      # Rails, will you please look in our vendor? kthx
+      initializer "bootsrap-sass.assets.precompile" do |app|
+        app.config.assets.precompile += %w(glyphicons-halflings.png glyphicons-halflings-white.png)
+      end
     end
   end
 end
