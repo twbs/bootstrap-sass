a714e88bf1ef34eb5e6ef9ef7205fa13288fb943
Gleb Mazovetskiy <glex.spb@gmail.com>
2014-04-18 21:45:55 +0200

    bump ruby gem to 3.1.1.1
    This is to fix a compatibility issue with sprockets + Rails >= 4.1.0, released on Apr 8.
    See https://github.com/twbs/bootstrap-sass/issues/578

--- lib/bootstrap-sass/version.rb
+++ lib/bootstrap-sass/version.rb
@@ -1,4 +1,4 @@
 module Bootstrap
-  VERSION = '3.1.1.0'
-  BOOTSTRAP_SHA = 'd08f7dd80711f6096f9d2f413ed2933f9d6dd2bf'
+  VERSION = '3.1.1.1'
+  BOOTSTRAP_SHA = '573ad10a9dba52e4a6f059e63b667f4034ad2b39'
 end
