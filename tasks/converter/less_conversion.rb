7eed336d8a3470db109ee952b7bea9a8e4bb7ed4
Gleb Mazovetskiy <glex.spb@gmail.com>
2014-06-16 22:40:24 +0200

    relative depend_on_asset path

--- tasks/converter/less_conversion.rb
+++ tasks/converter/less_conversion.rb
@@ -111,7 +111,7 @@ class Converter
           when 'thumbnails.less', 'labels.less', 'badges.less'
             file = extract_nested_rule file, 'a&'
           when 'glyphicons.less'
-            file = bootstrap_font_files.map { |p| %Q(//= depend_on_asset "bootstrap/#{File.basename(p)}") } * "\n" + "\n" + file
+            file = bootstrap_font_files.map { |p| %Q(//= depend_on_asset "../../fonts/bootstrap/#{File.basename(p)}") } * "\n" + "\n" + file
             file = replace_rules(file, '@font-face') { |rule|
               rule = replace_all rule, /(\$icon-font(?:-\w+)+)/, '#{\1}'
               replace_asset_url rule, :font
