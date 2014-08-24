c02deccd879848b2908714448a7d2c0840cef6d6
Gleb Mazovetskiy <glex.spb@gmail.com>
2014-08-24 22:59:30 +0200

    tests: clean sass cache before CompilationTest
    also update sass 3.4 and compass 1

--- test/compilation_test.rb
+++ test/compilation_test.rb
@@ -6,6 +6,7 @@ class CompilationTest < Minitest::Test
   def test_compilation
     path = 'assets/stylesheets'
     %w(_bootstrap bootstrap/_theme).each do |file|
+      FileUtils.rm_rf('.sass-cache', secure: true)
       engine = Sass::Engine.for_file("#{path}/#{file}.scss", syntax: :scss, load_paths: [path])
       FileUtils.mkdir_p("tmp/#{File.dirname(file)}")
       File.open("tmp/#{file}.css", 'w') { |f|
