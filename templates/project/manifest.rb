4e41a47ea5f6ba70248eacab3967838f3fead06e
Jan Stevens <jan@playpass.be>
2015-08-19 21:35:58 +0200

    Updated the templates

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -9,9 +9,7 @@ stylesheet '_bootstrap-variables.sass', :to => '_bootstrap-variables.sass'
 # Copy JS and fonts
 manifest = Pathname.new(File.dirname(__FILE__))
 assets   = File.expand_path('../../assets', manifest)
-{:javascript => 'javascripts',
- :font       => 'fonts'
-}.each do |method, dir|
+{:javascript => 'javascripts'}.each do |method, dir|
   root = Pathname.new(assets).join(dir)
   Dir.glob root.join('**', '*.*') do |path|
     path = Pathname.new(path)
