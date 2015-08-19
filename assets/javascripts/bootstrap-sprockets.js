7397065e0daf73ac84e0846b0278736163b147a2
Jan Stevens <jan@playpass.be>
2015-08-19 21:34:56 +0200

    New assets update for bootstrap 4

--- assets/javascripts/bootstrap-sprockets.js
+++ assets/javascripts/bootstrap-sprockets.js
@@ -1,11 +1,10 @@
-//= require ./bootstrap/affix
 //= require ./bootstrap/alert
 //= require ./bootstrap/button
 //= require ./bootstrap/carousel
 //= require ./bootstrap/collapse
 //= require ./bootstrap/dropdown
 //= require ./bootstrap/tab
-//= require ./bootstrap/transition
+//= require ./bootstrap/util
 //= require ./bootstrap/scrollspy
 //= require ./bootstrap/modal
 //= require ./bootstrap/tooltip
