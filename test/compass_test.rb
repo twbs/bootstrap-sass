418073fd435446c2c3d55e1e29990bbff4159c96
Gleb Mazovetskiy <glex.spb@gmail.com>
2018-04-17 23:35:26 +0100

    Fix test/compass_test.rb for recent bundler

--- test/compass_test.rb
+++ test/compass_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class CompassTest < Minitest::Test
   def test_create_project
     command = 'rm -rf tmp/new-compass-project; bundle exec compass create tmp/new-compass-project -r bootstrap-sass --using bootstrap --trace --force'
-    success = silence_stdout_if(!ENV['VERBOSE']) { system(command) }
+    success = silence_stdout_if(!ENV['VERBOSE']) { Bundler.with_clean_env { system(command) } }
     assert success, 'Compass project creation failed!'
   end
 end
