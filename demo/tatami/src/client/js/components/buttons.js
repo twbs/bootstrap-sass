5ec81abdf1192d204b0dc002d152bca707e26d6e
Isshu Rakusai <rakusai@kamilabo.jp>
2018-10-17 22:02:17 +0900

    button groupsをdemoに追加 (#13)
    btn-groupをモダンブラウザ対応
    
    left autoも不要
    
    Split button dropdownsをdemoに追加

--- demo/tatami/src/client/js/components/buttons.js
+++ demo/tatami/src/client/js/components/buttons.js
@@ -21,7 +21,7 @@ export default function Buttons () {
 
       <br />
       <br />
-      <h4>Size variation</h4>
+      <h3>Sizes</h3>
       <p>
         <button className='btn btn-primary btn-lg'>Large button</button>&nbsp;
         <button className='btn btn-default btn-lg'>Large button</button>
@@ -40,8 +40,13 @@ export default function Buttons () {
       </p>
 
       <br />
+      <h4>Block level button</h4>
+      <a href='javascript:;' className='btn btn-default btn-lg btn-block'>Block level button</a>&nbsp;
+      <a href='javascript:;' className='btn btn-primary btn-lg btn-block'>Block level button</a>&nbsp;
+
       <br />
-      <h4>Disabled</h4>
+      <br />
+      <h3>Disabled state</h3>
       <button className='btn btn-default' disabled>
         Default
       </button>
@@ -53,6 +58,81 @@ export default function Buttons () {
       <button className='btn btn-danger' disabled>
         Danger
       </button>
+
+      <br />
+      <br />
+      <h3>Button groups</h3>
+      <div className='btn-group' role='group' aria-label='...'>
+        <button type='button' className='btn btn-default'>Left</button>
+        <button type='button' className='btn btn-default'>Middle</button>
+        <button type='button' className='btn btn-default'>Right</button>
+      </div>
+
+      <h4>Vertical</h4>
+      <div className='btn-group-vertical' role='group' aria-label='...'>
+        <button type='button' className='btn btn-default'>One</button>
+        <button type='button' className='btn btn-default'>Two</button>
+        <button type='button' className='btn btn-default'>Three</button>
+        <div className='btn-group' role='group' aria-label='...'>
+          <button type='button' className='btn btn-default dropdown-toggle' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+            Dropdown{' '}
+            <span className='caret'></span>
+          </button>
+          <ul className='dropdown-menu'>
+            <li><a href='#'>Dropdown link</a></li>
+            <li><a href='#'>Dropdown link</a></li>
+          </ul>
+        </div>
+        <button type='button' className='btn btn-default'>Four</button>
+      </div>
+
+      <h4>Toolbar</h4>
+      <div className='btn-toolbar' role='toolbar' aria-label='...'>
+        <div className='btn-group' role='group' aria-label='...'>
+          <button type='button' className='btn btn-default'>1</button>
+          <button type='button' className='btn btn-default'>2</button>
+          <button type='button' className='btn btn-default'>3</button>
+        </div>
+        <div className='btn-group' role='group' aria-label='...'>
+          <div className='btn-group' role='group'>
+            <button type='button' className='btn btn-default dropdown-toggle' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+              Dropdown{' '}
+              <span className='caret'></span>
+            </button>
+            <ul className='dropdown-menu'>
+              <li><a href='#'>Dropdown link</a></li>
+              <li><a href='#'>Dropdown link</a></li>
+            </ul>
+          </div>
+          <button type='button' className='btn btn-default'>4</button>
+          <button type='button' className='btn btn-default'>5</button>
+          <button type='button' className='btn btn-default'>6</button>
+        </div>
+      </div>
+
+      <h4>Small size</h4>
+      <div className='btn-group btn-group-sm' role='group' aria-label='...'>
+        <button type='button' className='btn btn-default'>Left</button>
+        <button type='button' className='btn btn-default'>Middle</button>
+        <button type='button' className='btn btn-default'>Right</button>
+      </div>
+
+      <h4>Justified</h4>
+      <div className='btn-group btn-group-justified' role='group' aria-label='...'>
+        <button type='button' className='btn btn-default'>Left</button>
+        <button type='button' className='btn btn-default'>Middle</button>
+        <button type='button' className='btn btn-default'>Right</button>
+        <div className='btn-group' role='group'>
+          <button type='button' className='btn btn-default dropdown-toggle' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+            Dropdown{' '}
+            <span className='caret'></span>
+          </button>
+          <ul className='dropdown-menu'>
+            <li><a href='#'>Dropdown link</a></li>
+            <li><a href='#'>Dropdown link</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
--- demo/tatami/src/client/js/components/dropdowns.js
+++ demo/tatami/src/client/js/components/dropdowns.js
@@ -35,6 +35,35 @@ export default function Dropdowns () {
         </ul>
       </div>
 
+      <br />
+      <br />
+
+      <div className='btn-group'>
+        <button type='button' className='btn btn-danger'>Action</button>
+        <button type='button' className='btn btn-danger dropdown-toggle' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+          <span className='caret'></span>
+          <span className='sr-only'>Toggle Dropdown</span>
+        </button>
+        <ul className='dropdown-menu'>
+          <li><a href='#'>Action</a></li>
+          <li><a href='#'>Another action</a></li>
+          <li><a href='#'>Something else here</a></li>
+          <li role='separator' className='divider'></li>
+          <li><a href='#'>Separated link</a></li>
+        </ul>
+      </div>
+      {' '}
+      <div className='btn-group dropup'>
+        <button type='button' className='btn btn-default'>Dropup</button>
+        <button type='button' className='btn btn-default dropdown-toggle' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+          <span className='caret'></span>
+          <span className='sr-only'>Toggle Dropdown</span>
+        </button>
+        <ul className='dropdown-menu'>
+          <li><a href='#'>Action</a></li>
+          <li><a href='#'>Another action</a></li>
+        </ul>
+      </div>
     </div>
   )
 }
