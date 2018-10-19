875ac45b2fa86f1b8e849e6506a96c383d8b6e4c
Isshu Rakusai <rakusai@kamilabo.jp>
2018-10-19 15:24:56 +0900

    Labelはinline-blockにする (#16)

--- demo/tatami/src/client/js/components/labels.js
+++ demo/tatami/src/client/js/components/labels.js
@@ -4,13 +4,23 @@ export default function Labels () {
   return (
     <div>
       <h2>Labels</h2>
-      <span className='label label-default'>Default</span>{' '}
-      <a href='javascript:;' className='label label-default'>Link</a>{' '}
+      In the text <span className='label label-default'>Default</span>{' '}
       <span className='label label-primary'>Primary</span>{' '}
       <span className='label label-success'>Success</span>{' '}
       <span className='label label-info'>Info</span>{' '}
       <span className='label label-warning'>Warning</span>{' '}
       <span className='label label-danger'>Danger</span>
+
+      <br />
+      <br />
+
+      <h4>As links</h4>
+      In the text <a href='javascript:;' className='label label-default'>Default</a>{' '}
+      <a href='javascript:;' className='label label-primary'>Primary</a>{' '}
+      <a href='javascript:;' className='label label-success'>Success</a>{' '}
+      <a href='javascript:;' className='label label-info'>Info</a>{' '}
+      <a href='javascript:;' className='label label-warning'>Warning</a>{' '}
+      <a href='javascript:;' className='label label-danger'>Danger</a>
     </div>
   )
 }
