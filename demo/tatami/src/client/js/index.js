399b894ef29266850301b25f1e624ff23228f6e7
Takeru Yoshihara <takeru@notainc.com>
2019-02-14 16:18:29 +0900

    import @notainc/mouse-hover-visible

--- demo/tatami/src/client/js/index.js
+++ demo/tatami/src/client/js/index.js
@@ -6,6 +6,7 @@ import React from 'react'
 import {render} from 'react-dom'
 
 import '@notainc/key-focus-visible'
+import '@notainc/mouse-hover-visible'
 // import 'what-input'
 
 import App from './components/app'
