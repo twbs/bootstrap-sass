01ecf1a179d5fd834bd823193abbcc2ca5e2cf25
Isshu Rakusai <rakusai@kamilabo.jp>
2019-03-01 11:06:22 +0900

    Tatami type (#36)
    * heading typeを指定
    
    * gray-darkを使っている文字色はtext-colorを使う
    
    * heading line-heightは1.25にする
    
    * headingはline-heightを8の倍数にfont-sizeは7の倍数に
    
    * headings-line-heightの計算式のコメント

--- demo/tatami/src/client/js/components/type.js
+++ demo/tatami/src/client/js/components/type.js
@@ -3,18 +3,28 @@ import React from 'react'
 export default function Type () {
   return (
     <div>
-      <h1>h1. Bootstrap heading</h1>
-      <h2>h2. Bootstrap heading</h2>
-      <h3>h3. Bootstrap heading</h3>
-      <h4>h4. Bootstrap heading</h4>
+      <h1><strong>h1. Far far away, behind the word mountains</strong></h1>
+      <h1>h1. Far far away, behind the word mountains</h1>
+      <h2><strong>h2. Far far away, behind the word mountains</strong></h2>
+      <h2>h2. Far far away, behind the word mountains</h2>
+      <h3><strong>h3. Far far away, behind the word mountains</strong></h3>
+      <h3>h3. Far far away, behind the word mountains</h3>
+      <h4><strong>h4. Far far away, behind the word mountains</strong></h4>
+      <h4>h4. Far far away, behind the word mountains</h4>
       <p>Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula.</p>
       <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec ullamcorper nulla non metus auctor fringilla. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla.</p>
       <p>Maecenas sed diam eget risus varius blandit sit amet non magna. Donec id elit non mi porta gravida at eget metus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+      <h5><strong>h5. Far far away, behind the word mountains</strong></h5>
+      <h5>h5. Far far away, behind the word mountains</h5>
+      <h6><strong>h6. Far far away, behind the word mountains</strong></h6>
+      <h6>h6. Far far away, behind the word mountains</h6>
+      <small><strong>Small text</strong></small><br />
+      <small>Small text</small>
+
       <div>
         <p><a href='javascript:;'>Here is a text link</a></p>
         <p><button className='btn btn-link'>Button link</button></p>
       </div>
-
 
       <br />
       <br />
