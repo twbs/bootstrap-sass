32588c88076a98d9cd16fb8e976c37a1cfd9ccaf
Isshu Rakusai <rakusai@kamilabo.jp>
2019-03-13 16:57:07 +0900

    Tatami modal (#44)
    .alert-dismissibleの内容を消す
    
    文字列変更
    
    formのhas error stateの色が崩れるのでalertは別にする
    
    closeボタンの色とサイズを調整
    
    single qoute
    
    stateを共通化する
    
    fix form attr
    
    nav, nav-tabにデザインを適用
    
    justifiedの特殊cssは不要
    
    nav-tabs-justifiedを完全に削除
    
    nav-tabs-justifiedの変数も削除
    
    nav-tabs-justifiedまだ残っていた
    
    navの基本文字色とtabのhover色を変更
    
    list groupというものを発見したのでnavに追加してみた
    
    nab-lgのtabがすこしline-heightが大きすぎた
    
    marginを見るため、formのmarginも15-> 16pxにする
    
    nav-stacked-fixedを追加
    
    Tatami modalを仮実装
    
    header-no-borderを作る
    
    paddingは16から20pxにしてすこし広く
    
    Sizesにsmall modalを追加
    
    lgサイズは900pxだった
    
    ModalのCloseボタンを32pxと大きくする

--- demo/tatami/src/client/js/components/modals.js
+++ demo/tatami/src/client/js/components/modals.js
@@ -1,20 +1,87 @@
 import React, {Component} from 'react'
-import SampleModal from './sample-modal'
+import {
+  SampleModal1,
+  SampleModal2,
+  SampleModal3,
+  SampleModalSmall,
+  SampleModalSmall2,
+  SampleModalSmall3,
+  SampleModalSmall4,
+  SampleModalLarge } from './sample-modals'
 
 // Touch Device Friendly Button
 export default class Modals extends Component {
   constructor () {
     super()
-    this.state = {showModal: false}
+    this.state = {
+      showModal1: false,
+      showModal2: false,
+      showModalSmall: false,
+      showModalSmall2: false,
+      showModalSmall3: false,
+      showModalSmall4: false,
+      showModalLarge: false
+    }
   }
 
   render () {
     return (
       <div>
-        <h2>Modals</h2>
-        <SampleModal show={this.state.showModal} onHide={() => this.setState({showModal: false})} />
-        <button className='btn btn-default' onClick={() => this.setState({showModal: true})}>
-          Show Modal
+        <h1>Modals</h1>
+        <h3>Sizes</h3>
+
+        <SampleModal2 show={this.state.showModal2} onHide={() => this.setState({showModal2: false})} />
+        <button className='btn btn-default' onClick={() => this.setState({showModal2: true})}>
+          Default modal
+        </button>
+
+        <br /><br />
+
+        <SampleModalSmall show={this.state.showModalSmall} onHide={() => this.setState({showModalSmall: false})} />
+        <button className='btn btn-default' onClick={() => this.setState({showModalSmall: true})}>
+          Small modal
+        </button>
+
+        <br /><br />
+
+        <SampleModalLarge show={this.state.showModalLarge} onHide={() => this.setState({showModalLarge: false})} />
+        <button className='btn btn-lg btn-default' onClick={() => this.setState({showModalLarge: true})}>
+          Large modal
+        </button>
+
+        <h3>Use cases</h3>
+
+        <SampleModalSmall2 show={this.state.showModalSmall2} onHide={() => this.setState({showModalSmall2: false})} />
+        <button className='btn btn-default' onClick={() => this.setState({showModalSmall2: true})}>
+          Delete?
+        </button>
+
+        <br /><br />
+
+        <SampleModalSmall4 show={this.state.showModalSmall4} onHide={() => this.setState({showModalSmall4: false})} />
+        <button className='btn btn-default' onClick={() => this.setState({showModalSmall4: true})}>
+          Set password
+        </button>
+
+        <br /><br />
+
+        <SampleModal1 show={this.state.showModal1} onHide={() => this.setState({showModal1: false})} />
+        <button className='btn btn-default' onClick={() => this.setState({showModal1: true})}>
+          Upgrade induction
+        </button>
+
+        <br /><br />
+
+        <SampleModal3 show={this.state.showModal4} onHide={() => this.setState({showModal4: false})} />
+        <button className='btn btn-default' onClick={() => this.setState({showModal4: true})}>
+          Edit account info
+        </button>
+
+        <br /><br />
+
+        <SampleModalSmall3 show={this.state.showModalSmall3} onHide={() => this.setState({showModalSmall3: false})} />
+        <button className='btn btn-lg btn-default' onClick={() => this.setState({showModalSmall3: true})}>
+          Delete? (Mobile version)
         </button>
       </div>
     )
---
+++ demo/tatami/src/client/js/components/sample-modals.js
@@ -0,0 +1,159 @@
+/* eslint-env browser */
+
+import React from 'react'
+import {Modal} from 'react-bootstrap'
+
+export function SampleModal1({ show, onHide }) {
+  return (
+    <Modal show={show} onHide={onHide}>
+      <Modal.Header closeButton>
+        <h3 className='modal-title'><strong>Some induction title</strong></h3>
+      </Modal.Header>
+      <Modal.Body>
+        Instantly browse, search, and collect all your captures with unlimited access when you upgrade to Gyazo Pro, including this one.
+
+        <div style={ {  width: 292, height: 168, background: '#d8d8d8', margin: '24px auto 0' } } />
+      </Modal.Body>
+      <Modal.Footer>
+        <button className='btn btn-primary' onClick={onHide}>Upgrade</button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
+
+export function SampleModal2({ show, onHide }) {
+  return (
+    <Modal show={show} onHide={onHide}>
+      <Modal.Header closeButton>
+        <Modal.Title><strong>Modal title</strong></Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        Modal body text goes here. Max width is 600px.
+      </Modal.Body>
+      <Modal.Footer>
+        <button className='btn btn-primary' onClick={onHide}>Button</button>
+        <button className='btn btn-default' onClick={onHide}>Button</button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
+
+export function SampleModal3({ show, onHide }) {
+  return (
+    <Modal show={show} onHide={onHide}>
+      <Modal.Header closeButton>
+        <h3 className='modal-title'><strong>Edit account info</strong></h3>
+      </Modal.Header>
+      <form>
+        <Modal.Body>
+          <div className='form-group'>
+            <label className='control-label' htmlFor='exampleInput1'>Profile image</label>
+            <input type='text' className='form-control' id='exampleInput1' placeholder='Type something' />
+          </div>
+          <div className='form-group'>
+            <label className='control-label' htmlFor='exampleInput2'>Name</label>
+            <input type='text' className='form-control' id='exampleInput2' placeholder='Type something' />
+          </div>
+          <div className='form-group'>
+            <label className='control-label' htmlFor='exampleInputEmail1'>Email</label>
+            <input type='email' className='form-control' id='exampleInputEmail1' placeholder='Type something' />
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <button className='btn btn-primary' onClick={onHide}>Button</button>
+          <button className='btn btn-default' onClick={onHide}>Button</button>
+        </Modal.Footer>
+      </form>
+    </Modal>
+  )
+}
+
+export function SampleModalSmall({ show, onHide }) {
+  return (
+    <Modal show={show} onHide={onHide} dialogClassName='modal-sm'>
+      <Modal.Header className='modal-header-no-border'>
+        <h3 className='modal-title'><strong>Small modal</strong></h3>
+      </Modal.Header>
+      <Modal.Body>
+        Max width is 300px.
+      </Modal.Body>
+      <Modal.Footer>
+        <button className='btn btn-primary' onClick={onHide}>Button</button>
+        <button className='btn btn-default' onClick={onHide}>Button</button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
+
+export function SampleModalSmall2({ show, onHide }) {
+  return (
+    <Modal show={show} onHide={onHide} dialogClassName='modal-sm'>
+      <Modal.Header className='modal-header-no-border'>
+        <h3 className='modal-title'><strong>Delete?</strong></h3>
+      </Modal.Header>
+      <Modal.Body>
+        There is no undo for this action.
+      </Modal.Body>
+      <Modal.Footer>
+        <button className='btn btn-primary' onClick={onHide}>Copy link</button>
+        <button className='btn btn-default' onClick={onHide}>Delete now</button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
+
+export function SampleModalSmall3({ show, onHide }) {
+  return (
+    <Modal show={show} onHide={onHide} dialogClassName='modal-sm'>
+      <Modal.Header className='modal-header-no-border'>
+        <h3 className='modal-title text-center'><strong>Delete?</strong></h3>
+      </Modal.Header>
+      <Modal.Body>
+        There is no undo for this action.
+      </Modal.Body>
+      <Modal.Footer className='modal-footer-vertical'>
+        <button className='btn btn-primary btn-block btn-lg' onClick={onHide}>Copy link</button>
+        <button className='btn btn-default btn-block btn-lg' onClick={onHide}>Delete now</button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
+
+export function SampleModalSmall4({ show, onHide }) {
+  return (
+    <Modal show={show} onHide={onHide} dialogClassName='modal-sm'>
+      <Modal.Header closeButton>
+        <h3 className='modal-title'><strong>Set password</strong></h3>
+      </Modal.Header>
+      <form>
+        <Modal.Body>
+          <div className='form-group'>
+            <label className='control-label' htmlFor='exampleInput1'>Password</label>
+            <input type='password' className='form-control' id='exampleInput1' />
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <button className='btn btn-primary' onClick={onHide}>Button</button>
+          <button className='btn btn-default' onClick={onHide}>Button</button>
+        </Modal.Footer>
+      </form>
+    </Modal>
+  )
+}
+
+export function SampleModalLarge({ show, onHide }) {
+  return (
+    <Modal show={show} onHide={onHide} dialogClassName='modal-lg'>
+      <Modal.Header closeButton>
+        <h3 className='modal-title'><strong>Large modal</strong></h3>
+      </Modal.Header>
+      <Modal.Body>
+        Max width is 900px.
+      </Modal.Body>
+      <Modal.Footer>
+        <button className='btn btn-primary' onClick={onHide}>Button</button>
+        <button className='btn btn-default' onClick={onHide}>Button</button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
